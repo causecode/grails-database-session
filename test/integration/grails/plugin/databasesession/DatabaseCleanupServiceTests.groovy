8a345aa228a185912c23e2538d807e1de3dc295e
Shashank Agrawal <shashank.agrawal@causecode.com>
2014-07-24 16:15:44 +0530

    Fixed test case.

--- test/integration/grails/plugin/databasesession/DatabaseCleanupServiceTests.groovy
+++ test/integration/grails/plugin/databasesession/DatabaseCleanupServiceTests.groovy
@@ -13,14 +13,14 @@ class DatabaseCleanupServiceTests extends GroovyTestCase {
 		def session = new PersistentSession(creationTime: 0, lastAccessedTime: System.currentTimeMillis())
 		session.id = 's1'
 		session.save(failOnError: true)
-		def attr = new PersistentSessionAttribute(session: session, name: 'a1').save(failOnError: true)
-		new PersistentSessionAttributeValue(attribute: attr, serialized: persistentSessionService.serializeAttributeValue('v1')).save(failOnError: true)
+		def attr = new PersistentSessionAttribute([sessionId: session.id, name: 'a1',
+			serialized: persistentSessionService.serializeAttributeValue('v1')]).save(failOnError: true)
 
 		session = new PersistentSession(creationTime: 0, lastAccessedTime: System.currentTimeMillis())
 		session.id = 's2'
 		session.save(failOnError: true)
-		attr = new PersistentSessionAttribute(session: session, name: 'a2').save(failOnError: true)
-		new PersistentSessionAttributeValue(attribute: attr, serialized: persistentSessionService.serializeAttributeValue('v2')).save(failOnError: true)
+		attr = new PersistentSessionAttribute([session: session, name: 'a2',
+			serialized: persistentSessionService.serializeAttributeValue('v2')]).save(failOnError: true)
 
 		session = new PersistentSession(creationTime: 0, lastAccessedTime: System.currentTimeMillis())
 		session.id = 's3'
@@ -33,7 +33,6 @@ class DatabaseCleanupServiceTests extends GroovyTestCase {
 
 		assertEquals 3, PersistentSession.count()
 		assertEquals 2, PersistentSessionAttribute.count()
-		assertEquals 2, PersistentSessionAttributeValue.count()
 
 		def session = PersistentSession.get('s1')
 		session.lastAccessedTime = System.currentTimeMillis() - 20000
@@ -43,7 +42,6 @@ class DatabaseCleanupServiceTests extends GroovyTestCase {
 
 		assertEquals 3, PersistentSession.count()
 		assertEquals 2, PersistentSessionAttribute.count()
-		assertEquals 2, PersistentSessionAttributeValue.count()
 
 		session.lastAccessedTime = System.currentTimeMillis() - 2000000
 		session.save(flush: true)
@@ -52,7 +50,6 @@ class DatabaseCleanupServiceTests extends GroovyTestCase {
 
 		assertEquals 2, PersistentSession.count()
 		assertEquals 1, PersistentSessionAttribute.count()
-		assertEquals 1, PersistentSessionAttributeValue.count()
 	}
 
 	private void flushAndClear() {
